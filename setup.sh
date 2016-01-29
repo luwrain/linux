5ba0196f5558fea74a1931565c670c8b47620524
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-01-29 10:03:52 +0600

    setup.sh clean up

--- setup.sh
+++ setup.sh
@@ -1,21 +1,24 @@
-#!/bin/sh
-# Creates ~/.luwrain and performs initial LUWRAIN setup there
+#!/bin/bash
+# Creates ~/.luwrain and performs initial LUWRAIN setup
+# Copyright 2012-2016 Michael Pozhidaev <michael.pozhidaev@gmail.com>
 
+echo Compiling JNI part
 cd jni
-make
+make &>> /tmp/.luwrain.make.log
 cd ..
 
+echo Creating '~'/.luwrain
 mkdir -p ~/.luwrain
 for i in app extensions registry; do
     mkdir -p ~/.luwrain/$i
 done 
 
+echo Copying files to '~'/.luwrain
 cp -r hsqldb ~/.luwrain
 cp -r registry/. ~/.luwrain/registry
 cp -r news/ru/. ~/.luwrain/registry/org/luwrain/pim/news/groups/
 
-cat luwrain.conf.def > ~/.luwrain.conf
-
+echo Writing exact path of hsqldb files to registry
 HSQLDB=~/.luwrain/hsqldb
 
 for i in news mail contacts; do
@@ -25,6 +28,7 @@ cat <<EOF > ~/.luwrain/registry/org/luwrain/pim/$i/storing/strings.txt
 "url" = "jdbc:hsqldb:file:$HSQLDB/$i"
 "login" = "sa"
 "passwd" = ""
-
 EOF
 done
+
+echo 'Installation completed successfully!'
