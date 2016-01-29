3407352651c3a512567d63a20aa37271fc5f5465
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-01-29 09:57:30 +0600

    luwrain.sh update
    - common.sh moved to luwrain.sh and significantly updated (no longer reads ~/.luwrain.conf)
    - luwrain.conf.def removed (but commands go to doc/tts-commands.txt)

--- luwrain.sh
+++ luwrain.sh
@@ -1,16 +1,30 @@
-#!/bin/sh -e
-
-ENV_LUWRAIN_HOME="$LUWRAIN_HOME"
-
-if [ -r ~/.luwrain.conf ]; then
-    . ~/.luwrain.conf
-fi
+#!/bin/bash -e
+# Copyright 2012-2016 Michael Pozhidaev <michael.pozhidaev@gmail.com>
+#
+# This file is part of the LUWRAIN.
+#
+# LUWRAIN is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# LUWRAIN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
 
 THIS="${0##*/}"
 
-if [ -n "$ENV_LUWRAIN_HOME" ]; then
-    LUWRAIN_HOME="$ENV_LUWRAIN_HOME"
-fi
+jars()
+{
+    if [ -d "$1" ]; then
+	find "$1" -iname '*.jar' |
+	while read l; do
+	    echo -n "$l:"
+	done
+	echo
+    fi
+}
 
 if [ -z "$LUWRAIN_HOME" ]; then
     if [ -e ~/luwrain/luwrain.sh ]; then
@@ -27,41 +41,22 @@ if [ -z "$LUWRAIN_LANG" ]; then
     LUWRAIN_LANG=en
 fi
 
-if [ -z "$LUWRAIN_SPEECH" ]; then
-    LUWRAIN_SPEECH=org.luwrain.linux.speech.command
-fi
-
-if [ -z "$LUWRAIN_SPEECH_COMMAND" ]; then
-    LUWRAIN_SPEECH_COMMAND='espeak | aplay'
-fi
-
-if [ -z "$LUWRAIN_INTERACTION" ]; then
-    LUWRAIN_INTERACTION=org.luwrain.interaction.javafx.JavaFxInteraction
-fi
-
 LUWRAIN_MAIN_CLASS=org.luwrain.core.Init
 LUWRAIN_USER_HOME_DIR=~
-LUWRAIN_APPS_DIR=~/.luwrain/app
+LUWRAIN_EXT_DIR=~/.luwrain/extensions
 LUWRAIN_REGISTRY_DIR=~/.luwrain/registry
 LUWRAIN_DATA_DIR="$LUWRAIN_HOME/data"
 LUWRAIN_JAR_DIR="$LUWRAIN_HOME/jar"
 LUWRAIN_LIB_DIR="$LUWRAIN_HOME/lib"
 LUWRAIN_JNI_DIR="$LUWRAIN_HOME/jni"
 
-jars()
-{
-    if [ -d "$1" ]; then
-	find "$1" -iname '*.jar' |
-	while read l; do
-	    echo -n "$l:"
-	done
-	echo
-    fi
-}
+for i in extensions registry; do
+mkdir -p ~/.luwrain/$i
+done
 
 CLASS_PATH=":$(jars "$LUWRAIN_LIB_DIR/.")"
 CLASS_PATH="$CLASS_PATH:$(jars "$LUWRAIN_JAR_DIR/.")"
-CLASS_PATH="$CLASS_PATH:$(jars "$LUWRAIN_APPS_DIR/.")"
+CLASS_PATH="$CLASS_PATH:$(jars "$LUWRAIN_EXT_DIR/.")"
 
 exec java \
 -cp "$CLASS_PATH" \
@@ -70,9 +65,6 @@ exec java \
 --registry-dir="$LUWRAIN_REGISTRY_DIR" \
 --lang="$LUWRAIN_LANG" \
 --os=org.luwrain.linux.Linux \
---speech="$LUWRAIN_SPEECH" \
---speech-command="$LUWRAIN_SPEECH_COMMAND" \
 --data-dir="$LUWRAIN_DATA_DIR" \
 --user-home-dir="$LUWRAIN_USER_HOME_DIR" \
---interaction="$LUWRAIN_INTERACTION" \
-"$@" &> ~/.luwrain.log
+"$@" &> ~/.luwrain/log
