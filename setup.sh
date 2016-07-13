10eb5d12ed16dcd16c4d8e667882d643dbb2080a
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-07-13 09:05:47 +0700

    setup.sh update

--- setup.sh
+++ setup.sh
@@ -2,23 +2,19 @@
 # Creates ~/.luwrain and performs initial LUWRAIN setup
 # Copyright 2012-2016 Michael Pozhidaev <michael.pozhidaev@gmail.com>
 
-echo Compiling JNI part
 cd jni
 make &> /tmp/.luwrain.make.log
 cd ..
 
-echo Creating '~'/.luwrain
 mkdir -p ~/.luwrain
 for i in extensions registry; do
     mkdir -p ~/.luwrain/$i
 done 
 
-echo Copying files to '~'/.luwrain
 cp -r hsqldb ~/.luwrain
 cp -r registry/. ~/.luwrain/registry
-cp -r news/ru/. ~/.luwrain/registry/org/luwrain/pim/news/groups/
+cp -r i18n/ru/. ~/.luwrain/registry/
 
-echo Writing exact path of hsqldb files to registry
 HSQLDB=~/.luwrain/hsqldb
 
 for i in news mail contacts; do
