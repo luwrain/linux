a24840da3e957310adf770b6cd131df62ceaa568
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-11-10 18:54:38 +0700

    New using of classpath with core.ClassPath hack

--- luwrain.sh
+++ luwrain.sh
@@ -5,17 +5,6 @@
 THIS="${0##*/}"
 LUWRAIN_MAIN_CLASS=org.luwrain.core.Init
 
-jars()
-{
-    if [ -d "$1" ]; then
-	find "$1" -iname '*.jar' |
-	while read l; do
-	    echo -n "$l:"
-	done
-	echo
-    fi
-}
-
 if [ -z "$LUWRAIN_HOME" ]; then
     if [ -e ~/luwrain/luwrain.sh ]; then
 	LUWRAIN_HOME=~/luwrain
@@ -30,20 +19,14 @@ fi
 if [ -z "$LUWRAIN_LANG" ]; then
     LUWRAIN_LANG="${LANG%%_*}"
 fi
-
 if ! [ -e "$LUWRAIN_HOME/i18n/$LUWRAIN_LANG" ]; then
     LUWRAIN_LANG=en
 fi
 
-LUWRAIN_JAR_DIR="$LUWRAIN_HOME/jar"
-LUWRAIN_LIB_DIR="$LUWRAIN_HOME/lib"
-CLASS_PATH=":$(jars "$LUWRAIN_LIB_DIR/.")"
-CLASS_PATH="$CLASS_PATH:$(jars "$LUWRAIN_JAR_DIR/.")"
-CLASS_PATH="$CLASS_PATH:$(jars "$LUWRAIN_USER_DATA_DIR/extensions/.")"
-
 cd "$LUWRAIN_HOME"
+
 exec java \
--cp "$CLASS_PATH" \
+-cp jar/luwrain.jar:jar/luwrain-base.jar \
 -Djava.library.path="$LUWRAIN_HOME" \
 "$LUWRAIN_MAIN_CLASS" \
 "$@" &> ~/.luwrain.log
