b35569b0a533238433c10cc586195e37e4d16d9f
Michael Pozhidaev <msp@altlinux.ru>
2017-03-29 09:19:03 +0700

    ubuntu-16.04:removing needless files from ISO root and proper .disk.info

--- ubuntu/16.04/iso/scripts/config.sh
+++ ubuntu/16.04/iso/scripts/config.sh
@@ -1,5 +1,14 @@
 
+
+LWRISO_VERSION=1.0.1
+LWRISO_ARCH="$(uname -m)"
+LWRISO_DATE="$(date +%Y%m%d)"
+
 LWRISO_NAMESERVER=192.168.1.1
 LWRISO_LANG=ru
 
 CHANNELS_DIR=$LWRISO_ROOT/opt/luwrain/i18n/ru/org/luwrain/speech/channels/
+
+if [ "$LWRISO_ARCH" == x86_64 ]; then
+    LWRISO_ARCH=amd64
+fi
