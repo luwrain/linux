20cfc17246915635fc9a169667b2f65682c006e3
Michael Pozhidaev <msp@altlinux.ru>
2017-04-10 12:59:09 +0700

    ubuntu-16.04:adding EN language support

--- ubuntu/16.04/iso/scripts/config.sh
+++ ubuntu/16.04/iso/scripts/config.sh
@@ -1,12 +1,10 @@
 
-
+LWRISO_LANG=en
 LWRISO_VERSION=1.0.1
 LWRISO_ARCH="$(uname -m)"
 LWRISO_DATE="$(date +%Y%m%d)"
 
 LWRISO_NAMESERVER=192.168.1.1
-LWRISO_LANG=ru
-
 CHANNELS_DIR=$LWRISO_ROOT/opt/luwrain/i18n/ru/org/luwrain/speech/channels/
 
 if [ "$LWRISO_ARCH" == x86_64 ]; then
